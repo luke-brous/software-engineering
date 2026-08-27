2b51dcf2438d9028c341a607843e6c14d17038aa
luke-brous <luke.broussard@gmail.com>
2026-08-27 16:05:07 -0500

    first ruby commit

---
+++ test/cat.rb
@@ -0,0 +1,17 @@
+class Cat
+  attr_accessor(:name, :age, :color)
+  def initialize(name, age, color)
+    @name = name
+    @age = age
+    @color = color
+  end
+
+  def meow
+    puts "Meow!"
+  end
+end
+
+cat1 = Cat.new("Whiskers", 3, "gray")
+cat1.meow
+
+puts cat1.name
